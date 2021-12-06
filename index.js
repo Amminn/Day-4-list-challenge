add54d2122a44960fe1ab01912338e4a3513abcd
Amminn <vrb.nnn@gmail.com>
2021-12-06 14:44:32 +0100

    fixing the styling

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let items = ["Candles", "Decorations", "Chocolate"]
 const checklist = document.querySelector(".item-list")
 const addBtn = document.querySelector(".add button")
+let input = document.getElementById("add-input");
+
 
 
 // save the items inside the array
-
 addBtn.addEventListener('click', function() {
-    let input = document.getElementById("add-input").value
-    items.push(input)
-    printing()
+    if (input.value === "") {
+        alert("Please fill the input first!")
+    } else {
+        items.push(input.value)
+        input.value = "";
+        printing()
+    }
 })
 
 function dlt(x) {
@@ -32,22 +37,14 @@ function dlt(x) {
 function printing() {
     checklist.innerHTML = ""
     for (let i = 0; i < items.length; i++) {
-        checklist.innerHTML += `
-        <div class="js-list">
-            <input type="checkbox">
-            <label class="strikethrough"><span>${items[i]}</span><button onclick="dlt(${i})"><i class="fas fa-trash"></i></button></label>
-        </div>
-        `
+        let fullInput = `<div class="js-list">
+        <input type="checkbox" id="${items[i]}">
+        <label for="${items[i]}" class="strikethrough"><span>${items[i]}</span></label>
+        <button onclick="dlt(${i})"><i class="fas fa-trash"></i></button>
+    </div>`
+        checklist.insertAdjacentHTML('beforeend',fullInput)
     }
 }
 
 printing()
-
-// Task:
-// - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
-// - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
-
-// Stretch goals:
-// - Add an input which allows the user to add more items.
-// - Add a delete button for the items.
 
